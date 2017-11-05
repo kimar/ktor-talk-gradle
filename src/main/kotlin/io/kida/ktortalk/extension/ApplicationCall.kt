cf4d240e8324922ebc0db3696bba38f7b2beef57
Marcus Kida <marcus@kida.io>
2017-11-05 16:48:42 +0100

    Add ContentType and StatusCode to ApplicationCall extension

--- src/main/kotlin/io/kida/ktortalk/extension/ApplicationCall.kt
+++ src/main/kotlin/io/kida/ktortalk/extension/ApplicationCall.kt
@@ -1,8 +1,10 @@
 package io.kida.ktortalk.extension
 
 import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
 
-suspend fun ApplicationCall.respondJson(obj: Any) {
-    respondText(obj.jsonString())
+suspend fun ApplicationCall.respondJson(obj: Any, contentType: ContentType? = null, statusCode: HttpStatusCode? = HttpStatusCode.OK) {
+    respondText(obj.jsonString(), contentType, statusCode)
 }
